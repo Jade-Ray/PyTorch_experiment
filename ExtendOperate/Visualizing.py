3559ba2f46fe07e84f1b2e359bdcb1dc590fcb31
雷琪 <33926387+Jade-Ray@users.noreply.github.com>
2021-07-24 10:44:13 +0800

    📃 docs(visualizing.py): review
    >💠Review the Tensorboard operation
    
    (⊙o⊙)

--- ExtendOperate/Visualizing.py
+++ ExtendOperate/Visualizing.py
@@ -1,3 +1,16 @@
+# %% [markdown]
+# # Visualizing models, data, and training with tensorboard
+# illustrates some of `TensorBoard` functionality
+# ---
+# - Read indata and with appropriate transforms
+# - Set up TensorBoard
+# - Write to TensorBoard
+# - Inspect a model architecture using TensorBoard
+# - Use TensorBoarrd to create interactive verrsions of the visualizations
+#   - a couple of ways to inspect our trraining data
+#   - how to track our model's perforrmance as it trains
+#   - how to assess our model's perfoormance once it is trained
+
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -9,17 +22,15 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 
-from torch.utils.tensorboard import SummaryWriter
-
 transform = transforms.Compose(
     [transforms.ToTensor(), 
     transforms.Normalize((0.5,), (0.5,))])
 
-trainset = torchvision.datasets.FashionMNIST('./Data/FashionMNIST', 
+trainset = torchvision.datasets.FashionMNIST('./data', 
     download=True,
     train=True, 
     transform=transform)
-testset = torchvision.datasets.FashionMNIST('./Data/FashionMNIST', 
+testset = torchvision.datasets.FashionMNIST('./data', 
     download=True,
     train=False, 
     transform=transform)
@@ -29,9 +40,12 @@ trainloader = torch.utils.data.DataLoader(trainset, batch_size=4,
 testloader = torch.utils.data.DataLoader(testset, batch_size=4, 
                                         shuffle=False, num_workers=0)
 
+# constant for classes
 classes = ('T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat', 
             'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle Boot')
 
+# helper function to show an image
+# (used in the `plot_classes_prerds` function below)
 def matplotlib_imshow(img, one_channel=False):
     if one_channel:
         img = img.mean(dim=0)
@@ -65,41 +79,79 @@ net = Net()
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
 
+# %% [markdown]
+# ***💠TensorBoard Setup***
+from torch.utils.tensorboard import SummaryWriter
+
+# default `log_dir` is "runs" - we'll be more specific here
 writer = SummaryWriter('runs/fashion_mnist_experiment_1')
 
+# %% [markdown]
+# ***💠Writing to TensorBoard***
+
+# get some random training images
 dataiter = iter(trainloader)
 images, labels = dataiter.next()
 
+# create grid of images
 img_grid = torchvision.utils.make_grid(images)
 
+# show images
 matplotlib_imshow(img_grid, one_channel=True)
 
+# write to tensorboard
 writer.add_image('four_fashion_mnist_images', img_grid)
-writer.add_graph(net, images)
-# writer.close()
 
+# %% [markdown]
+# ***💠Inspect the moodel using TensorBoard***
+writer.add_graph(net, images)
+writer.close()
+
+# %% [markdown]
+# ***💠Adding a "Projector" to TensorBoard*** 
+# can visualize the lower dimensional representation of higher dimensional data via the `add_embedding` method
+
+# helper function
 def select_n_random(data, labels, n=100):
+    '''
+    Selects n random datapoints and their corresponding labels from a dataset
+    '''
     assert len(data) == len(labels)
     perm = torch.randperm(len(data))
     return data[perm][:n], labels[perm][:n]
 
+# select random images and their target indices
 images, labels = select_n_random(trainset.data, trainset.targets)
 
+# get the class labels for each image
 class_labels = [classes[lab] for lab in labels]
 
-features = images.view(-1, 28 * 28)
+# log embeddings
+features = images.view(-1, 28 * 28) # [100, 784]
 writer.add_embedding(features, 
                     metadata=class_labels, 
                     label_img=images.unsqueeze(1))
 writer.close()
 
+# %% [markdown]
+# ***💠Tracking model training with TensorBoard***
+# log the runnig loss to TensorBoard with a view into the predictionss the model via `plot_classes_preds` function.
+
+# helper functions   
 def images_to_probs(net, images):
+    '''
+    Generates predictions and corresponding probabilites from a trained network and a list of images
+    '''
     output = net(images)
+    # convert output probabilities to predicted class
     _, preds_tensor = torch.max(output, 1)
     preds = np.squeeze(preds_tensor.numpy())
     return preds, [F.softmax(el, dim=0)[i] for i, el in zip(preds, output)]
 
 def plot_classes_preds(net, images, labels):
+    '''
+    Generates matplotlib Figure using a trained network, along with images and labels from a batch, that shows the network's top prediction along with its probability, alongside the actual label, coloring this information based on whether the prediction was correct or not. Uses to "images_to_probs" function. 
+    '''
     preds, probs = images_to_probs(net, images)
     fig = plt.figure(figsize=(12, 48))
     for idx in np.arange(4):
@@ -113,10 +165,14 @@ def plot_classes_preds(net, images, labels):
     return fig
 
 running_loss = 0.0
-for epoch in range(1):
+for epoch in range(1):  # loop over the dataset multiple times
     for i, data in enumerate(trainloader, 0):
         inputs, labels = data
+        
+        # zero the parameter gradients
         optimizer.zero_grad()
+        
+        # forward + backward + optimize
         outputs = net(inputs)
         loss = criterion(outputs, labels)
         loss.backward()
@@ -124,39 +180,50 @@ for epoch in range(1):
 
         running_loss += loss.item()
         if i % 1000 == 999:
+            # ...log the running loss
             writer.add_scalar('training loss', 
                             running_loss / 1000, 
                             epoch * len(trainloader) + i)
+            # ...log a Matplotlib Figure showing the model's predictions on a random mini-batch
             writer.add_figure('predictions vs. actuals', 
                             plot_classes_preds(net, inputs, labels), 
                             global_step=epoch * len(trainloader) + i)
             running_loss = 0.0
 print('Finished Training')
 
+# %% [markdown]
+# ***💠Assessing trained models with TensorBoard***
+# - gets the probability prerdictions in a test_size x num_classes Tensor
+# - gets the preds in a test_size Tensor
 class_probs = []
-class_preds = []
+class_label = []
 with torch.no_grad():
     for data in testloader:
         images, labels = data
         output = net(images)
         class_probs_batch = [F.softmax(el, dim=0) for el in output]
-        _, class_preds_batch = torch.max(output, 1)
+
         class_probs.append(class_probs_batch)
-        class_preds.append(class_preds_batch)
+        class_label.append(labels)
 
 test_probs = torch.cat([torch.stack(batch) for batch in class_probs])
-test_preds = torch.cat(class_preds)
+test_preds = torch.cat(class_label)
 
+# helperer function
 def add_pr_curve_tensorboard(class_index, test_probs, test_preds, global_step=0):
-    tensorboard_preds = test_preds == class_index
+    '''
+    Takes in a "class_index" from 0 to 9 and plots the corresponding precision-recall curve
+    '''
+    tensorboard_truth = test_preds == class_index
     tensorboard_probs = test_probs[:, class_index]
 
     writer.add_pr_curve(classes[class_index], 
-                        tensorboard_preds, 
+                        tensorboard_truth, 
                         tensorboard_probs, 
                         global_step=global_step)
     writer.close()
 
+# plot all the pr curves
 for i in range(len(classes)):
     add_pr_curve_tensorboard(i, test_probs, test_preds)
 
